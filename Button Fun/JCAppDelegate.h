0aee7c007ff9a0be1ff79b11f6828b1b7dfecff7
Jonathan Costello <jocostello@epocrates.com>
2013-04-01 17:01:27 -0400

    Initial Commit

---
+++ Button Fun/JCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JCAppDelegate.h
+//  Button Fun
+//
+//  Created by Costello, Jonathan on 4/1/13.
+//  Copyright (c) 2013 Costello, Jonathan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Button Fun/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Button Fun
+//
+//  Created by Costello, Jonathan on 4/1/13.
+//  Copyright (c) 2013 Costello, Jonathan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JCAppDelegate class]));
+    }
+}
