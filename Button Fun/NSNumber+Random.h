362df6c45e162ff366fd1e7a418dd325d5331bc0
Jonathan Costello <jocostello@epocrates.com>
2013-04-09 10:01:46 -0400

    Removed Utility class, and added method to a category on NSNumber.

---
+++ Button Fun/NSNumber+Random.h
@@ -0,0 +1,13 @@
+//
+//  NSNumber+Random.h
+//  Button Fun
+//
+//  Created by Costello, Jonathan on 4/9/13.
+//  Copyright (c) 2013 Costello, Jonathan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSNumber (Random)
+
+@end
---
+++ Button Fun/NSNumber+Random.m
@@ -0,0 +1,13 @@
+//
+//  NSNumber+Random.m
+//  Button Fun
+//
+//  Created by Costello, Jonathan on 4/9/13.
+//  Copyright (c) 2013 Costello, Jonathan. All rights reserved.
+//
+
+#import "NSNumber+Random.h"
+
+@implementation NSNumber (Random)
+
+@end
